c1a6c0aebf8939a98cc81c82402c3388e915635c
Sujit K M <kmsujit@gmail.com>
2023-12-14 12:39:58 +0530

    Experiment to bring Map Reduce word count problem to Lexical Analysis. Untested to the experiment stated in the README.

---
+++ go.mod
@@ -0,0 +1,17 @@
+module map-reduce/results
+
+go 1.21.5
+
+require github.com/BWbwchen/MapReduce v1.0.0
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
+	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210828152312-66f60bf46e71 // indirect
+	google.golang.org/grpc v1.42.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ map-reduce.go
@@ -0,0 +1,43 @@
+package results
+import (
+	"strings"
+	"strconv"	
+
+	"github.com/BWbwchen/MapReduce/worker"
+)
+
+type MapReduce struct {
+	mapped map[string]int
+	index int
+}
+ 
+var s *MapReduce
+
+
+ // creating a function to the class
+func generator(key string) int {
+	var keyindex int
+	if(s.mapped == nil){
+		s.mapped = make(map[string]int)
+		s.index = 1
+	}
+	keyindex, ok := s.mapped[key]
+	if(!ok){
+		s.mapped[key] = s.index
+		s.index++
+	}
+
+	return keyindex
+ }
+
+func Map(key string, value string, ctx worker.MrContext){
+	split := strings.Fields(value)
+    for _, val := range split {
+		ctx.EmitIntermediate(val, strconv.Itoa(generator(val)))
+	}    	
+}
+
+func Reduce(key string, values []string, ctx worker.MrContext){
+    ctx.Emit(key, strconv.Itoa(s.mapped[key]))
+}
+
