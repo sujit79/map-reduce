760abeb83e7a658a2e3535b52af7e438bf1eda25
Sujit K M <kmsujit@gmail.com>
2025-08-14 17:42:03 +0530

    made the program compatible to using -buildmode=plugin
    made the program compatible to using -buildmode=plugin
    
    Now you dont need a main program to run the map-reduce, also you could define your own master and worker nodes.
    
    go run -race cmd/master.go -i 'txt/*' -p 'cmd/wc.so' -r 1 -w 8 &
    sleep 1
    go run -race cmd/worker.go -i 'txt/*' -p 'cmd/wc.so' -r 1 -w 1 &

--- map-reduce.go
+++ map-reduce.go
@@ -1,43 +1,42 @@
-package results
+package main
+
 import (
+	"strconv"
 	"strings"
-	"strconv"	
 
 	"github.com/BWbwchen/MapReduce/worker"
 )
 
 type MapReduce struct {
 	mapped map[string]int
-	index int
+	index  int
 }
- 
+
 var s *MapReduce
 
-
- // creating a function to the class
+// creating a function to the class
 func generator(key string) int {
 	var keyindex int
-	if(s.mapped == nil){
+	if s.mapped == nil {
 		s.mapped = make(map[string]int)
 		s.index = 1
 	}
 	keyindex, ok := s.mapped[key]
-	if(!ok){
+	if !ok {
 		s.mapped[key] = s.index
 		s.index++
 	}
 
 	return keyindex
- }
+}
 
-func Map(key string, value string, ctx worker.MrContext){
+func Map(key string, value string, ctx worker.MrContext) {
 	split := strings.Fields(value)
-    for _, val := range split {
+	for _, val := range split {
 		ctx.EmitIntermediate(val, strconv.Itoa(generator(val)))
-	}    	
+	}
 }
 
-func Reduce(key string, values []string, ctx worker.MrContext){
-    ctx.Emit(key, strconv.Itoa(s.mapped[key]))
+func Reduce(key string, values []string, ctx worker.MrContext) {
+	ctx.Emit(key, strconv.Itoa(s.mapped[key]))
 }
-
